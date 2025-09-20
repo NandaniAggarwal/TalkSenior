01c58cc56e89a12c013d98d3cccf066262d723e3
NandaniAggarwal <nandaniaggarwal0109@gmail.com>
2025-09-20 12:57:52 +0530

    updated cors origin

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app= express();
 
 app.use(express.json())
 app.use(cors({
-  origin: 'http://localhost:5173'  // Update this in production to frontend URL
+  origin: ['http://localhost:5173', 'https://talksenior-1.onrender.com'] // Update this in production to frontend URL
 }));
 
 app.get('/',(req,res)=>{
