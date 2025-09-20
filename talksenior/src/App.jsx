c17a42965b9798c955b247846972a3e0f659a574
NandaniAggarwal <nandaniaggarwal0109@gmail.com>
2025-09-20 13:41:54 +0530

    imported redirect

--- talksenior/src/App.jsx
+++ talksenior/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { Redirect } from 'react-router-dom/cjs/react-router-dom'
 import { Button } from '@chakra-ui/react'
 import { Route,Switch } from 'react-router-dom/cjs/react-router-dom'
 import Homepage from './Pages/Homepage'
